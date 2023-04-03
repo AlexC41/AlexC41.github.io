4a4187bb91dee8bc75b580298d9504d06a3a28f6
AlexC41 <alexandru.ciuta.21@gmail.com>
2023-04-04 01:35:49 +0300

    #home CSS updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector(".hamburger-lines");  
-const menu_links = document.querySelector(".menu_links");  
+const menu_links = document.querySelector(".menu-links");  
 
 hamburger.addEventListener('click', () => {  
     hamburger.classList.toggle("change")  
-    menu_links.classList.toggle("menu_links_move")  
+    menu_links.classList.toggle("menu-links-move")  
 });  
