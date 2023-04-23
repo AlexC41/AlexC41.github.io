2e521a4c56c981e0558d0be17780f0fda1f56947
AlexC41 <alexandru.ciuta.21@gmail.com>
2023-04-24 00:50:11 +0300

    Updates
    - new resources added -> icons for contact
    - 2 files removed
    - tab's title modified from "document" to "Portfolio"
    - some optimization on all sections > div removed
    - contact section added

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const menu_links = document.querySelector(".nav-items");
 hamburger.addEventListener('click', () => {  
     hamburger.classList.toggle("change")  
     menu_links.classList.toggle("nav-items-move")  
-}); 
+});
